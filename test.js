51b4e2997ed5a312e8eced723b530300b75cde83
VladimirKoshovec <koshovec.vladimir@gmail.com>
2022-09-17 11:13:27 +0300

    +

--- test.js
+++ test.js
@@ -2,6 +2,8 @@
 dog = document.getElementById ("data-dog");
 score = document.getElementById ("data-score");
 frame = document.getElementById("data-frame");
+start = document.getElementById("data-start");
+stopt = document.getElementById("data-stop");
 
 const frameSizeW  = getComputedStyle(frame).width;
 const frameSizeH  = getComputedStyle(frame).height;
@@ -11,22 +13,39 @@ const dogSizeH = getComputedStyle(dog).height;
 
 let dogTop;
 let dogLeft;
+let intervalname;
+let scoreInt = 0;
 
 function dogPosition() {
-dogTop =  Math.round ( Math.random() * (parseInt(frameSizeH) - 10) + 10 - parseInt(dogSizeH));
-dogLeft =  Math.round ( Math.random() * (parseInt(frameSizeW) - 10) + 10- parseInt(dogSizeW));
+   dogTop =  Math.round ( Math.random() * (parseInt(frameSizeH) - 10) + 10 - parseInt(dogSizeH));
+   dogLeft =  Math.round ( Math.random() * (parseInt(frameSizeW) - 10) + 10- parseInt(dogSizeW));
 
-dog.style.left = dogLeft + "px";
-dog.style.top = dogTop + "px";
+   dog.style.left = dogLeft + "px";
+   dog.style.top = dogTop + "px";
 }
 
-setInterval (dogPosition, 500);
+// ----------------events---------------
+
+start.addEventListener("click", startdog);
+stopt.addEventListener("click", stopdog);
+dog.addEventListener("click", heatdog);
 
 
+function startdog(){
+    intervalname = setInterval (dogPosition, 500);
+}
+
+function stopdog(){
+    clearInterval(intervalname);
+}
+
+function heatdog(){
+    scoreInt += 1;
+    frame.innerHtml = scoreInt;
+    //console.log(scoreInt);
+}
 
 
-console.log(dogLeft);
-console.log(dogTop);
 
 
 
